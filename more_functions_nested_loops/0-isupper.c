5c1ceded665dfba7dc1fc595da81adc664624ca4
quliyevadinara <quliyevadinara14@gmail.com>
2025-12-07 19:36:39 +0000

    betty-style fixed

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -4,11 +4,11 @@
  * _isupper - checks for uppercase character
  * @c: character to check
  *
- * Return: 1 if c is uppercase, 0 otherwise
+ * Return: 1 if uppercase, 0 otherwise
  */
 int _isupper(int c)
 {
-    if (c >= 'A' && c <= 'Z')
-        return (1);
-    return (0);
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	return (0);
 }
