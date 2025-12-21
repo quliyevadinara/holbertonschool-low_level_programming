eae977c5865ccfdcb608a642d5944b180d7504f3
quliyevadinara <quliyevadinara14@gmail.com>
2025-12-21 18:32:25 +0000

    Task 0-4 completed

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the number of arguments passed to the program
+ * @argc: The number of command line arguments
+ * @argv: An array containing the command line arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
